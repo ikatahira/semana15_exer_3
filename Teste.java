e2d33fbf069b6cbb41889198141ae65242ee2193
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-11-18 17:27:39 +0000

    exer3

---
+++ Teste.java
@@ -0,0 +1,17 @@
+public class Teste {
+    
+    public static void main(String[] args) {
+        
+        Aluno aluno=new Aluno("java",7,8,9);
+        Aluno aluno2=new Aluno(7,5,3);
+        Aluno aluno3=new Aluno();
+      
+         if(aluno.aprovado()){
+
+        }
+        else{
+
+        }
+
+    }
+}
